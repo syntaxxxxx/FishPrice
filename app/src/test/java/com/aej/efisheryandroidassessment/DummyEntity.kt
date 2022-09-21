322af5ee80b73a25aaaf15402e301522670675f2
macbook <fiqrihafzainislami@gmail.com>
2022-09-21 08:16:50 +0700

    adding dummy entity domain modele

--- app/src/test/java/com/aej/efisheryandroidassessment/DummyEntity.kt
+++ app/src/test/java/com/aej/efisheryandroidassessment/DummyEntity.kt
@@ -1,9 +1,23 @@
 package com.aej.efisheryandroidassessment
 
+import com.aej.efisheryandroidassessment.domain.FishDomainModel
 import com.aej.efisheryandroidassessment.entity.FishDtoBean
 
 val dummyListFishDtoBean = listOf(
     FishDtoBean(
+        uuid = "721636c6-80ea-48ad-94ac-f663e49b0e39",
+        commodity = "FIQRI",
+        provinceArea = "LAMPUNG",
+        cityArea = "BANDAR LAMPUNG",
+        size = "20",
+        price = "7400",
+        parsedDate = "2022-01-04T23:54:44Z",
+        timestamp = "1641340484994"
+    )
+)
+
+val dummyListFishDomainModel = listOf(
+    FishDomainModel(
         uuid = "721636c6-80ea-48ad-94ac-f663e49b0e39",
         commodity = "FIQRI",
         provinceArea = "LAMPUNG",

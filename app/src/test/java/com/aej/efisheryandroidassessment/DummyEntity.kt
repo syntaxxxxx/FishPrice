1cd14585b02d9684074883a5d7bf078f68d91ad7
macbook <fiqrihafzainislami@gmail.com>
2022-09-21 08:09:00 +0700

    adding dummy entity fish price purpose for testing

---
+++ app/src/test/java/com/aej/efisheryandroidassessment/DummyEntity.kt
@@ -0,0 +1,16 @@
+package com.aej.efisheryandroidassessment
+
+import com.aej.efisheryandroidassessment.entity.FishDtoBean
+
+val dummyListFishDtoBean = listOf(
+    FishDtoBean(
+        uuid = "721636c6-80ea-48ad-94ac-f663e49b0e39",
+        commodity = "FIQRI",
+        provinceArea = "LAMPUNG",
+        cityArea = "BANDAR LAMPUNG",
+        size = "20",
+        price = "7400",
+        parsedDate = "2022-01-04T23:54:44Z",
+        timestamp = "1641340484994"
+    )
+)

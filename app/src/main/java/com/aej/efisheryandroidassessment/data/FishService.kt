aaeff2483c330199dd056717b472ed3fbdccb381
macbook <fiqrihafzainislami@gmail.com>
2022-09-21 13:41:47 +0700

    moved package

--- app/src/main/java/com/aej/efisheryandroidassessment/data/FishService.kt
+++ app/src/main/java/com/aej/efisheryandroidassessment/data/FishService.kt
@@ -1,6 +1,6 @@
 package com.aej.efisheryandroidassessment.data
 
-import com.aej.efisheryandroidassessment.entity.FishDtoBean
+import com.aej.efisheryandroidassessment.data.entity.FishDtoBean
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 

f20eab21f193115d1b1630e4175829f792119847
macbook <fiqrihafzainislami@gmail.com>
2022-09-21 13:41:19 +0700

    adding dummy ui model list

--- app/src/test/java/com/aej/efisheryandroidassessment/DummyEntity.kt
+++ app/src/test/java/com/aej/efisheryandroidassessment/DummyEntity.kt
@@ -1,7 +1,8 @@
 package com.aej.efisheryandroidassessment
 
-import com.aej.efisheryandroidassessment.domain.FishDomainModel
-import com.aej.efisheryandroidassessment.entity.FishDtoBean
+import com.aej.efisheryandroidassessment.domain.entity.FishDomainModel
+import com.aej.efisheryandroidassessment.data.entity.FishDtoBean
+import com.aej.efisheryandroidassessment.presentation.FishUiModel
 
 val dummyListFishDtoBean = listOf(
     FishDtoBean(
@@ -18,6 +19,19 @@ val dummyListFishDtoBean = listOf(
 
 val dummyListFishDomainModel = listOf(
     FishDomainModel(
+        uuid = "721636c6-80ea-48ad-94ac-f663e49b0e39",
+        commodity = "FIQRI",
+        provinceArea = "LAMPUNG",
+        cityArea = "BANDAR LAMPUNG",
+        size = "20",
+        price = "7400",
+        parsedDate = "2022-01-04T23:54:44Z",
+        timestamp = "1641340484994"
+    )
+)
+
+val dummyFishUiModelList = listOf(
+    FishUiModel(
         uuid = "721636c6-80ea-48ad-94ac-f663e49b0e39",
         commodity = "FIQRI",
         provinceArea = "LAMPUNG",

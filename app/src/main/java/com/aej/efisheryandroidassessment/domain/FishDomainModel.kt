4d4a6869376c67a9603deab2417f1e0178c3c84b
macbook <fiqrihafzainislami@gmail.com>
2022-09-21 08:08:26 +0700

    adding fish price response

---
+++ app/src/main/java/com/aej/efisheryandroidassessment/domain/FishDomainModel.kt
@@ -0,0 +1,12 @@
+package com.aej.efisheryandroidassessment.domain
+
+data class FishDomainModel(
+    val uuid: String? = null,
+    val commodity: String? = null,
+    val provinceArea: String? = null,
+    val cityArea: String? = null,
+    val size: String? = null,
+    val price: String? = null,
+    val parsedDate: String? = null,
+    val timestamp: String? = null,
+)
---
+++ app/src/main/java/com/aej/efisheryandroidassessment/entity/FishDtoBean.kt
@@ -0,0 +1,30 @@
+package com.aej.efisheryandroidassessment.entity
+
+import com.aej.efisheryandroidassessment.domain.FishDomainModel
+import com.google.gson.annotations.SerializedName
+
+data class FishDtoBean(
+    @SerializedName("uuid") val uuid: String? = null,
+    @SerializedName("komoditas") val commodity: String? = null,
+    @SerializedName("area_provinsi") val provinceArea: String? = null,
+    @SerializedName("area_kota") val cityArea: String? = null,
+    @SerializedName("size") val size: String? = null,
+    @SerializedName("price") val price: String? = null,
+    @SerializedName("tgl_parsed") val parsedDate: String? = null,
+    @SerializedName("timestamp") val timestamp: String? = null,
+)
+
+fun FishDtoBean.mapToFishDomainModel(): FishDomainModel =
+    FishDomainModel(
+        uuid = uuid,
+        commodity = commodity,
+        provinceArea = provinceArea,
+        cityArea = cityArea,
+        size = size,
+        price = price,
+        parsedDate = parsedDate,
+        timestamp = timestamp
+    )
+
+fun List<FishDtoBean>.mapToFishDomainModel(): List<FishDomainModel> =
+    map { it.mapToFishDomainModel() }

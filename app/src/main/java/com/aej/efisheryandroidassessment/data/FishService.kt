6dce2535e4207ce77dd6a884882f3db85e50aa26
macbook <fiqrihafzainislami@gmail.com>
2022-09-21 08:05:48 +0700

    provide api service

---
+++ app/src/main/java/com/aej/efisheryandroidassessment/data/FishService.kt
@@ -0,0 +1,12 @@
+package com.aej.efisheryandroidassessment.data
+
+import com.aej.efisheryandroidassessment.entity.FishDtoBean
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+
+interface FishService {
+
+    @GET("/list")
+    fun fishPrice(): Single<List<FishDtoBean>>
+
+}

86102988f3f70c436c11685a84145d1de905c32c
macbook <fiqrihafzainislami@gmail.com>
2022-09-22 18:51:58 +0700

    setup background action bar

--- app/src/main/java/com/aej/efisheryandroidassessment/common/presentation/BaseActivity.kt
+++ app/src/main/java/com/aej/efisheryandroidassessment/common/presentation/BaseActivity.kt
@@ -1,15 +1,21 @@
 package com.aej.efisheryandroidassessment.common.presentation
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.viewbinding.ViewBinding
 import com.aej.efisheryandroidassessment.presentation.FishViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
-    
+
+    protected var viewBinding: VB? = null
+
     private var toolbar: Toolbar? = null
 
     protected val disposable by lazy { CompositeDisposable() }
@@ -18,11 +24,13 @@ abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(setupViewBinding().root)
+        viewBinding = setupViewBinding()
+        setContentView(viewBinding?.root)
         setupToolbar()
         setupBackButton()
         setupViews(savedInstanceState)
         setupOnClickListener()
+        setupBackgroundActionBar()
     }
 
     abstract fun setupViewBinding(): VB
@@ -40,6 +48,16 @@ abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
                 setDisplayHomeAsUpEnabled(setupBackButton())
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setupBackgroundActionBar() {
+        window.statusBarColor = ContextCompat.getColor(
+            applicationContext,
+            android.R.color.white
+        )
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 
     override fun onDestroy() {

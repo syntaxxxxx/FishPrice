f0800fe84f3f89bb50777ef31b5f3ad83e193c4c
macbook <fiqrihafzainislami@gmail.com>
2022-09-22 09:46:16 +0700

    adding query param

--- app/src/main/java/com/aej/efisheryandroidassessment/presentation/FishViewModel.kt
+++ app/src/main/java/com/aej/efisheryandroidassessment/presentation/FishViewModel.kt
@@ -3,14 +3,15 @@ package com.aej.efisheryandroidassessment.presentation
 import com.aej.efisheryandroidassessment.common.presentation.BaseViewModel
 import com.aej.efisheryandroidassessment.domain.FishUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import retrofit2.http.Query
 import javax.inject.Inject
 
 @HiltViewModel
 class FishViewModel @Inject constructor(private val useCase: FishUseCase) :
     BaseViewModel<FishUiState>() {
 
-    fun fishPrice() {
-        useCase.execute(Unit)
+    fun fishPrice(query: String) {
+        useCase.execute(FishUseCase.Params(query = query))
             .doOnSubscribe { singleLiveEvent.value = FishUiState.ShowLoading }
             .doOnTerminate { singleLiveEvent.value = FishUiState.HideLoading }
             .subscribe({ userList ->
@@ -24,7 +25,9 @@ class FishViewModel @Inject constructor(private val useCase: FishUseCase) :
         if (fishList.isEmpty()) {
             singleLiveEvent.value = FishUiState.ShowEmpty
         } else {
-            singleLiveEvent.value = FishUiState.Success(fishList)
+            fishList.filter { it.uuid != null }.map { fishUiModel ->
+                singleLiveEvent.value = FishUiState.Success(fishUiModel)
+            }
         }
     }
 

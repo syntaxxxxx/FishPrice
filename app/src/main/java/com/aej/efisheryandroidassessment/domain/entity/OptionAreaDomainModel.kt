b3d47142ccdfb8497015cfd220ea8bc93cde778a
macbook <fiqrihafzainislami@gmail.com>
2022-09-22 19:19:20 +0700

    mapping domain entity to ui model entity

--- app/src/main/java/com/aej/efisheryandroidassessment/domain/entity/OptionAreaDomainModel.kt
+++ app/src/main/java/com/aej/efisheryandroidassessment/domain/entity/OptionAreaDomainModel.kt
@@ -1,6 +1,17 @@
 package com.aej.efisheryandroidassessment.domain.entity
 
+import com.aej.efisheryandroidassessment.presentation.entity.OptionAreaUiModel
+
 data class OptionAreaDomainModel(
     val province: String? = null,
     val city: String? = null,
 )
+
+fun OptionAreaDomainModel.mapToOptionAreaUiModel(): OptionAreaUiModel =
+    OptionAreaUiModel(
+        province = province,
+        city = city
+    )
+
+fun List<OptionAreaDomainModel>.mapToOptionAreaUiModel(): List<OptionAreaUiModel> =
+    map { it.mapToOptionAreaUiModel() }

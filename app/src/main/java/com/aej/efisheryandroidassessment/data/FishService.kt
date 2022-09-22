6aa823ad43196a6c7a694c7cdfe42a8511d4f87d
macbook <fiqrihafzainislami@gmail.com>
2022-09-22 19:04:00 +0700

    adding option area endpoint to fish service

--- app/src/main/java/com/aej/efisheryandroidassessment/data/FishService.kt
+++ app/src/main/java/com/aej/efisheryandroidassessment/data/FishService.kt
@@ -1,7 +1,7 @@
 package com.aej.efisheryandroidassessment.data
 
 import com.aej.efisheryandroidassessment.data.entity.FishDtoBean
-import com.google.gson.JsonObject
+import com.aej.efisheryandroidassessment.data.entity.OptionAreaDtoBean
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,5 +12,8 @@ interface FishService {
     fun fishPrice(
         @Query("search") query: String
     ): Single<List<FishDtoBean>>
+
+    @GET("v1/storages/5e1edf521073e315924ceab4/option_area")
+    fun optionArea(): Single<List<OptionAreaDtoBean>>
 
 }

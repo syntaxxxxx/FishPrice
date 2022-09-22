edc322ddfe008b6499dd82ec283d5adddd083634
macbook <fiqrihafzainislami@gmail.com>
2022-09-22 19:07:34 +0700

    mapping data to domain model entity

--- app/src/main/java/com/aej/efisheryandroidassessment/data/entity/OptionAreaDtoBean.kt
+++ app/src/main/java/com/aej/efisheryandroidassessment/data/entity/OptionAreaDtoBean.kt
@@ -1,8 +1,19 @@
 package com.aej.efisheryandroidassessment.data.entity
 
+import com.aej.efisheryandroidassessment.domain.entity.OptionAreaDomainModel
 import com.google.gson.annotations.SerializedName
 
 data class OptionAreaDtoBean(
     @SerializedName("province") val province: String? = null,
     @SerializedName("city") val city: String? = null,
 )
+
+fun OptionAreaDtoBean.mapToOptionAreaDomainModel() =
+    OptionAreaDomainModel(
+        province = province,
+        city = city
+    )
+
+
+fun List<OptionAreaDtoBean>.mapToOptionAreaDomainModel(): List<OptionAreaDomainModel> =
+    map { it.mapToOptionAreaDomainModel() }

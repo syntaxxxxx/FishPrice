ba1982353f2dd5eacb0ebba11d9c08c38689254c
macbook <fiqrihafzainislami@gmail.com>
2022-09-22 18:57:26 +0700

    create view extensions

---
+++ app/src/main/java/com/aej/efisheryandroidassessment/common/utils/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.aej.efisheryandroidassessment.common.utils
+
+import android.view.View
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
